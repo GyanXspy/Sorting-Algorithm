2200bf99cee32c49159736adbf3d5d34120d51bb
Gyana Ranjan Patra <168622340+GyanXspy@users.noreply.github.com>
2024-10-17 17:40:20 +0530

    Update IterativeMethod.java

--- IterativeMethod.java
+++ IterativeMethod.java
@@ -86,6 +86,7 @@ public class IterativeMethod {
                 for (int i = 0; i < arr.length; i++) {
                     System.out.print(arr[i]+" ");
                 }
+                System.out.println();
                 System.out.println("No of comparision = "+c);
                 break;
             default:
